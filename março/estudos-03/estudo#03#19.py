a445e0fd25e04a08dd13631fd7a86bc37b894d02
Gabriel Dias <gabrielcarvalhodiaz.gz@gmail.com>
2022-03-20 11:00:10 -0300

    est1

--- março/estudos-03/estudo#03#19.py
+++ março/estudos-03/estudo#03#19.py
@@ -1,1 +1,12 @@
-class
+class  Pessoa:
+    def __init__(self, nome, peso, altura):
+        self.nome = nome
+        self.peso = peso
+        self.altura = altura
+
+    def mostrar(self):
+        print(self.nome, self.peso, self.altura)
+
+pessoa1 = Pessoa('gabriel', 500, 164)
+
+pessoa1.mostrar()
